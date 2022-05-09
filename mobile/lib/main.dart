8e36b4b20e52eaf265f7d4724b420ff9835f5f6d
FAUCITANO Liam <liam.faucitano@gmail.com>
2022-05-09 15:25:04 +0200

    [Management] Arrange classs

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,10 +1,7 @@
 // ignore_for_file: must_be_immutable
 
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
 import 'package:mobile/login.dart';
-import 'package:mobile/constants.dart';
-import 'package:mobile/notes_de_frais.dart';
 import 'package:mobile/feesheets_list.dart';
 // import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
@@ -22,28 +19,4 @@ void main() {
       },
     ),
   );
-}
-
-class DrawerContent extends StatelessWidget {
-  const DrawerContent({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-        child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        module(
-            page: Principale(php_session_id: 'none'),
-            text: 'Mes notes de frais en cours',
-            asset: 'assets/images/pen.json'),
-        module(
-            page: Principale(php_session_id: 'none'),
-            text: 'Notes de frais archivées',
-            asset: 'assets/images/dossier.json'),
-      ],
-    ));
-  }
 }
